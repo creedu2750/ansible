72c51c80a33b1c777e0d5ccc9d013f3839964cb3
creedu2750 <creedu2750@gmail.com>
2020-11-27 16:15:18 +0900

    ansible yml setting

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env bash
 
-sudo yum install epel-release -y
-sudo yum install ansible -y
+yum install epel-release -y
+yum install ansible -y
+
+mkdir -p /home/vagrant/.vim/autoload /home/vagrant/.vim/bundle
+touch /home/vagrant/.vimrc
+touch /home/vagrant/.bashrc
